4bf5c3975f93cf8f7f08428524e07a523ba75f36
dhckdwls <codetama51@gmail.com>
2023-12-20 14:19:12 +0900

    등록 후 번호가 바뀌게 구현

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class App {
 	private Scanner sc;
+	
 
 	public App(Scanner sc) {
 		this.sc = sc;
@@ -9,7 +10,9 @@ public class App {
 
 	public void run() {
 		System.out.println("== 명언 앱 실행 ==");
-
+		
+		int lastId = 1;
+		
 		while (true) {
 			System.out.print("명령어 ) ");
 			String cmd = sc.nextLine().trim();
@@ -21,7 +24,9 @@ public class App {
 				String content = sc.nextLine().trim();
 				System.out.print("작가 : ");
 				String author = sc.nextLine().trim();
-				System.out.println("1번 명언이 등록되었습니다.");
+				System.out.printf("%d번 명언이 등록되었습니다.",lastId);
+				lastId++;
+				
 			}
 		}
 
